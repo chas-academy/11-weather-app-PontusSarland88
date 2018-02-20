02ccc62060b5badaa6d6a18e972de7320da99eca
Pontus Särland <pontus.sarland@chasacademy.se>
2018-02-20 19:19:04 +0100

    now show wind, humidity and calculates sunrise and sunset

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -10,14 +10,17 @@ class Form extends Component {
 
     onSubmit(e) {
         e.preventDefault();
-
+        
         const cityName = e.nativeEvent.target.elements[0].value;
 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=5434f1c129e1ac657b10a23c1ac6a1e9`)
             .then(res => res.json())
             .then(res => {
                 this.setState({
-                    weather: res.weather
+                    weather: res.weather,
+                    wind: res.wind,
+                    main: res.main,
+                    sys: res.sys
                 }, function () {
                     console.log(this.state.weather);
                 });
@@ -26,7 +29,7 @@ class Form extends Component {
 
     render() {
         return (
-          <div>
+          <div className="container">
             <form onSubmit={this.onSubmit.bind(this)}>
               <input className="form-control" type="text" placeholder="Type the city name here" name="city" />
               <button className="btn btn-primary" type="submit">Get weather</button>
@@ -37,11 +40,35 @@ class Form extends Component {
                 <p>
                   {this.state.weather[0].description}
                 </p>
+                <table className="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td>Wind</td>
+                            <td>{this.state.wind.speed} m/s</td>
+                        </tr>
+                        <tr>
+                            <td>Humidity</td>
+                            <td>{this.state.main.humidity}%</td>
+                        </tr>
+                        <tr>
+                            <td>Sunrise</td>
+                            <td>{this.calculateTime(this.state.sys.sunrise)}</td>
+                        </tr>
+                        <tr>
+                            <td>Sunset</td>
+                            <td>{this.calculateTime(this.state.sys.sunset)}</td>
+                        </tr>
+                    </tbody>
+                </table>
               </div>
               : <p>No results yet</p>
             }
           </div>
         );
+    }
+    
+    calculateTime(time) {
+        return new Date(time * 1e3).toISOString().slice(-13, -5);
     }
 }
  
